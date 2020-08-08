2eaf8cbc81e5db3ffa0ab795678ac1fd6d3bf958
ivanpikulyk <68194099+ivanpikulyk@users.noreply.github.com>
2020-08-08 13:46:25 +0300

    Create mx-record-check.sh

---
+++ mx-record-check.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This function will check MX records of domain name, IP address of MX`s record and PTR`s of those IP`s.    
+
+mx() {
+
+        domainName=$(echo "$1" | cut -d'@' -f2)
+        echo -e "\n\n$domainName | cut -d'@' -f2"
+        echo "host -t MX $domainName | awk '{print \$7}'"
+        for MX in $(host -t MX "$domainName" | awk '{print $7}') 
+        do  
+           echo -e "\n\n$MX"
+           host "$MX"
+           for ip in $(host -t A "$MX" | awk '{print $4}')
+           do  
+              host "$ip"
+           done  
+        done
+            
+        echo -e "Do you want to sent ICMP request with command: "ping -c 3 "$MX"" [y/n]: "
+        
+        read input
+
+        if [[ "$input" == "y" ]]; then
+           for MX in $(host -t MX "$domainName" | awk '{print $7}') 
+           do  
+              ping -c 3 "$MX"
+           done
+        else
+            echo -e "Your answer is "$input", so this is the end of script work!"
+        fi  
+         
+}
